9805bef2b735c2bc2bcd72c673664f1e641f6e4f
Marco Mariotto <mari8.marco@gmail.com>
2020-06-11 22:57:56 +0200

    split work in more files

---
+++ policy.h
@@ -0,0 +1,11 @@
+#ifndef POLICY_H
+#define POLICY_H
+
+
+class Policy
+{
+public:
+    Policy();
+};
+
+#endif // POLICY_H
---
+++ segment.h
@@ -0,0 +1,11 @@
+#ifndef SEGMENT_H
+#define SEGMENT_H
+
+
+class Segment
+{
+public:
+    Segment();
+};
+
+#endif // SEGMENT_H
